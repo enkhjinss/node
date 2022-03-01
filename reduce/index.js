50524981e4107bb6611dc4591e22ac5fcc9ccf08
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-01 14:21:27 +0800

    promise uzsen

--- reduce/index.js
+++ reduce/index.js
@@ -108,16 +108,28 @@
 
 // users array object өгөгдсөн бол name -ын эхний үсгийг томруулах. (Pure function)
 
-const users = [
-{ id: 11, name: 'bat', age: 23, group: 'editor' },
-{ id: 47, name: 'bold', age: 28, group: 'admin' },
-{ id: 85, name: 'tulga', age: 34, group: 'editor' },
-{ id: 97, name: 'suren', age: 28, group: 'admin' }
-];
+// const users = [
+// { id: 11, name: 'bat', age: 23, group: 'editor' },
+// { id: 47, name: 'bold', age: 28, group: 'admin' },
+// { id: 85, name: 'tulga', age: 34, group: 'editor' },
+// { id: 97, name: 'suren', age: 28, group: 'admin' }
+// ];
 
-const result = users.map((val , i) => {
-    // const a = val.name[0].toUpperCase();
-    return val
-});
+// const result = users.map((val , i) => {
+//     // const a = val.name[0].toUpperCase();
+//     return val
+// });
 
-console.log(result);
+// console.log(result);
+
+
+//===============================================================>
+
+const validName = (name) => {
+    const n =  name.split(" ");
+    n.forEach((el) => {
+        
+    });
+}
+
+console.log(validName("M. Wells"));
