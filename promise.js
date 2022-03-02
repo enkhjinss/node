c1c4392c6cfafa99afb28e8110aeeb6e7b442e84
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-02 08:27:47 +0800

    prmise daalgavar

--- promise.js
+++ promise.js
@@ -1,41 +1,41 @@
-// ================================================> daalgavar 1
+// // ================================================> daalgavar 1
 
-const testPromise = (data) => {
-    return new Promise((resolve, reject) => {
-        if (typeof data === "number") {
-            if (data % 2 === 0) {
-                setTimeout(() => resolve("even"), 2000);
-            } else {
-                setTimeout(() => reject("odd"), 1000);
-            }
-        } else {
-            reject("error");
-        }
-    });
-};
+// const testPromise = (data) => {
+//     return new Promise((resolve, reject) => {
+//         if (typeof data === "number") {
+//             if (data % 2 === 0) {
+//                 setTimeout(() => resolve("even"), 2000);
+//             } else {
+//                 setTimeout(() => reject("odd"), 1000);
+//             }
+//         } else {
+//             reject("error");
+//         }
+//     });
+// };
 
-testPromise(53)
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error); // error
-    });
-testPromise(10)
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error); // even 2sec after
-    });
+// testPromise(53)
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error); // error
+//     });
+// testPromise(10)
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error); // even 2sec after
+//     });
 
-testPromise(9)
-    .then((result) => {
-        console.log(result); // odd 1sec after
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+// testPromise(9)
+//     .then((result) => {
+//         console.log(result); // odd 1sec after
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
 
 // ============================================================> daalgavar 2
@@ -43,20 +43,40 @@ testPromise(9)
 
 const fs = require('fs');
 
-const promise1 = fs.readFile('./word_count/big.txt', 'utf8', function(err, data){
-    // console.log(data);
-});
-const promise2 = fs.readFile('./word_count/sgb-words.txt', 'utf8', function(err, data){
-    // console.log(data);
-});
-const promise3 = fs.readFile('./word_count/shakespeare.txt', 'utf8', function(err, data){
-    // console.log(data);
-});
-const promise4 = fs.readFile('./word_count/smaller.txt', 'utf8', function(err, data){
-    // console.log(data);
+const files = ["./words/test1.txt" , "./words/test2.txt" , "./words/test3.txt"]
+
+const promises = [];
+files.forEach((file , index) => {
+    let promise;
+    fs.readFile(`./${file}` , 'utf8' , ((err , data) => {
+        promise = data;
+        console.log(data);
+    }));
+    promises.push(promise);
 });
 
-Promise.all([promise1 , promise2 , promise3 , promise4]).then((text) => {
+console.log(promises)
+git status
+
+
+
+
+Promise.all(promises).then((text) => {
     console.log(text);
 })
+
+
+
+// const promise1 = fs.readFile('./word_count/big.txt', 'utf8', function(err, data){
+//     // console.log(data);
+// });
+// const promise2 = fs.readFile('./word_count/sgb-words.txt', 'utf8', function(err, data){
+//     // console.log(data);
+// });
+// const promise3 = fs.readFile('./word_count/shakespeare.txt', 'utf8', function(err, data){
+//     // console.log(data);
+// });
+// const promise4 = fs.readFile('./word_count/smaller.txt', 'utf8', function(err, data){
+//     // console.log(data);
+// });
 
